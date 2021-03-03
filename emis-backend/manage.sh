7a21ac389651a424fb9d0e065e4b8f8c9983c0be
Simon Davy <simon.davy@thedatalab.org>
2021-03-03 14:13:34 +0000

    Add osrelease install scripting

--- emis-backend/manage.sh
+++ emis-backend/manage.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 ./scripts/install.sh
 ./scripts/update-users.sh developers emis-backend/researchers emis-backend/reviewers
 ./scripts/jobrunner.sh emis-backend
+./osrelease/setup.sh
 
 for f in /srv/jobrunner/environ/*.env; do
     # shellcheck disable=SC1090
