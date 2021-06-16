04403566c6d09ab7cd6b6a5fd0015a63465d238c
bloodearnest <bloodearnest@gmail.com>
2021-06-16 12:38:08 +0100

    bump timeout for GH run

--- tests/jobrunner.sh
+++ tests/jobrunner.sh
@@ -15,7 +15,7 @@ export PYTHONPATH=/srv/jobrunner/code:/srv/jobrunner/lib
 python3 -m jobrunner.add_job https://github.com/opensafely/research-template generate_study_population
 " | su - jobrunner -c bash
 
-cat << EOF | timeout 12s bash
+cat << EOF | timeout 20s bash
 until journalctl -u jobrunner | grep -q 'Completed successfully project=research-template action=generate_study_population'
 do
     sleep 2
