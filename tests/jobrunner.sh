881616c4a5b6dbb10c7c1c16a572051b90f85195
bloodearnest <bloodearnest@gmail.com>
2021-06-16 13:22:30 +0100

    better test failure message on timeout

--- tests/jobrunner.sh
+++ tests/jobrunner.sh
@@ -15,9 +15,17 @@ export PYTHONPATH=/srv/jobrunner/code:/srv/jobrunner/lib
 python3 -m jobrunner.add_job https://github.com/opensafely/research-template generate_study_population
 " | su - jobrunner -c bash
 
-cat << EOF | timeout 20s bash
+code=0
+cat << EOF | timeout 20s bash || code=$?
 until journalctl -u jobrunner | grep -q 'Completed successfully project=research-template action=generate_study_population'
 do
     sleep 2
 done
 EOF
+
+if test "$code" == "124"; then
+    echo "Timeout waiting for job to complete"
+fi
+
+exit $code
+
