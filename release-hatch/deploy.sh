63703b3bfe626aac93a57f8414707bb5f894be23
Tom Ward <tomward@fmail.co.uk>
2021-09-16 16:13:46 +0100

    fix: more renaming hatch -> release-hatch

--- release-hatch/deploy.sh
+++ release-hatch/deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # start the docker container
-DIR=~jobrunner/hatch
+DIR=~jobrunner/release-hatch
 echo "Pulling image"
 docker-compose --no-ansi -f $DIR/docker-compose.yaml pull --quiet release-hatch
 docker-compose --no-ansi -f $DIR/docker-compose.yaml up --detach release-hatch
--- release-hatch/install.sh
+++ release-hatch/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-DIR=~jobrunner/hatch
-cp -a ./hatch "$DIR"
+DIR=~jobrunner/release-hatch
+cp -a ./release-hatch "$DIR"
 chown -R jobrunner:jobrunner "$DIR"
 chmod -R go-rwx "$DIR"
 
-systemctl enable "$DIR/hatch.service"
-systemctl enable "$DIR/hatch.timer"
-systemctl start hatch.timer
-systemctl start hatch.service || { journalctl -u hatch.service; exit 1; }
+systemctl enable "$DIR/release-hatch.service"
+systemctl enable "$DIR/release-hatch.timer"
+systemctl start release-hatch.timer
+systemctl start release-hatch.service || { journalctl -u release-hatch.service; exit 1; }
