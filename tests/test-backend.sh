d27aaf2a7112eab15a9d5813493a34db8ab11c8a
Tom Ward <tomward@fmail.co.uk>
2021-10-13 16:24:31 +0100

    attempt to run the test

--- tests/test-backend.sh
+++ tests/test-backend.sh
@@ -31,16 +31,18 @@ test "$(id -g jobrunner)" == "10000"
 #     echo "job-runner auto-update failed"
 # fi
 
+# install jobrunner test script
+
+cp tests/jobrunner-docker.sh /srv/jobrunner/
+
 # TODO: run tests/jobrunner-docker.sh inside the jobrunner docker container
 docker-compose -f ~jobrunner/job-runner/docker-compose.yaml logs job-runner
 docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner pwd
 docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner ls -lh 
 docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner ls -lh /app/workdir/
-docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner ls -lh /app/workdir/tests/
-docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner ls -lh /app/workdir/tests/jobrunner-docker.sh
+docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner ls -lh /app/workdir/jobrunner-docker.sh
+docker-compose -f ~jobrunner/job-runner/docker-compose.yaml exec -T job-runner /app/workdir/jobrunner-docker.sh
 exit 1
-
-# /app/workdir/tests/jobrunner-docker.sh
 
 
 # run release-hatch tests
