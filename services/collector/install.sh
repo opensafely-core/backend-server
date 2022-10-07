32bac7821ce7fc40856ac2b6b70b3be07886c63c
Simon Davy <simon.davy@thedatalab.org>
2022-10-07 15:10:10 +0100

    Apply suggestions from code review
    Co-authored-by: Tom Ward <tomward@fmail.co.uk>

--- services/collector/install.sh
+++ services/collector/install.sh
@@ -8,7 +8,7 @@ BIN=$DIR/collector
 
 cp services/collector/* "$DIR"
 
-# curl sometimes complains about overwriting files, to download to tmp
+# curl sometimes complains about overwriting files, so download to tmp
 curl -s https://github-proxy.opensafely.org/opensafely-core/backend-server/releases/download/v0.1/otelcol_hny_linux_amd64 -o /tmp/collector
 mv /tmp/collector "$BIN"
 chmod +x "$BIN"
