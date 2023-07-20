35edf5380840359dba5499153d23685174ba8f1c
Tom Ward <tomward@fmail.co.uk>
2023-07-20 16:06:30 +0100

    chore: pass shellcheck

--- tpp-backend/current_queries.sh
+++ tpp-backend/current_queries.sh
@@ -4,9 +4,9 @@ verbose=${1:-}
 tmp=$(mktemp)
 for job in $(docker ps -f label=org.opensafely.action=cohortextractor | grep -oE "os-job-.*")
 do
-  docker logs --tail 10000 $job |& tac | grep -Em 1 '^2022-' -B10000 | tac > $tmp || true
-  ts=$(grep -oE "202.-..-.. ..:..:..:" $tmp)
+  docker logs --tail 10000 "$job" |& tac | grep -Em 1 '^2022-' -B10000 | tac > "$tmp" || true
+  ts=$(grep -oE "202.-..-.. ..:..:..:" "$tmp")
   delta=$(python3 -c "from datetime import datetime; d = datetime.now() - datetime.fromisoformat(\"$ts\"); print(d)")
   echo "$ts: $job: $delta"
-  test -n "$verbose" && { cat $tmp; echo; }
+  test -n "$verbose" && { cat "$tmp"; echo; }
 done
