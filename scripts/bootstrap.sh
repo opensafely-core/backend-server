69d931d2361c4fb2976efed031f523d6acff00e2
bloodearnest <simon.davy@thedatalab.org>
2023-08-17 12:40:52 +0100

    Enforce a single canonical location
    The checkout of the backed-server repo should be /srv/backend-server
    
    Fixes #143

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+BACKEND_SERVER_DIR="${BACKEND_SERVER_DIR:-/srv/backend-server}"
+
+if test "$PWD" != "$BACKEND_SERVER_DIR"; then
+  echo "You must run this script from a checkout of backend-server located at $BACKEND_SERVER_DIR"
+  exit 1
+fi
+
 if test -z "$1"
 then
   echo "Please specify your backend when running this file. For example:"
-  echo "./bootstrap.sh my_backend";
+  echo "$0 my_backend";
   exit 1
 fi
 
@@ -12,7 +19,8 @@ echo "BACKEND_JUST=$1" > .env
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]
 then
-  mv bin/just-1.14.0-x86_64-unknown-linux-musl /usr/local/bin/just
+  echo "Installing just in /usr/local/bin/just"
+  cp "$PWD/bin/just-1.14.0-x86_64-unknown-linux-musl" /usr/local/bin/just
   chmod 0755 /usr/local/bin/just
 else
   echo "Only linux-gnu is currently supported for installing 'just'."
