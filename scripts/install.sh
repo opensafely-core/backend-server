fb27d2e6991182da7bac47d95af4a1218ec36343
bloodearnest <simon.davy@thedatalab.org>
2023-09-12 11:57:03 +0100

    Update login banner and user messaging
    This adds a better login banner, as well as a user hint for developers
    about the shared user account.
    
    Also includes and obnoxious warning about recent changes.

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,6 +32,19 @@ ln -sf /etc/opensafely/profile /etc/profile.d/opensafely.sh
 ln -sf /etc/opensafely/sudoers /etc/sudoers.d/opensafely
 ln -sf /etc/opensafely/ssh.conf /etc/ssh/sshd_config.d/99-opensafely.conf
 
+chmod 0755 /etc/opensafely/{banner,bashrc}
+
+# add login banner
+
+ln -sf /etc/opensafely/banner /etc/update-motd.d/00-opensafely-banner
+# add our bashrc config
+if ! grep -q "etc/opensafely/bashrc" /etc/bash.bashrc; then
+    echo "test -f /etc/opensafely/bashrc && . /etc/opensafely/bashrc" >> /etc/bash.bashrc
+fi
+
+# reduce motd noise
+chmod a-x /etc/update-motd.d/{10-help-text,50-motd-news,90-updates-available,91-contract-ua-esm-status,91-release-upgrade}
+
 grep -q "^UMASK.*027" /etc/login.defs || sed -i 's/^UMASK.*$/UMASK 027/' /etc/login.defs
 
 systemctl reload ssh
