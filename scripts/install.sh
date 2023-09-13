ed727fdb175b3da8fa7d83d81edc214142efbd71
bloodearnest <simon.davy@thedatalab.org>
2023-09-13 14:48:52 +0100

    A few fixes from testing the rename user process

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,25 +26,29 @@ systemctl disable rsync
 
 # copy our files
 cp -a --no-preserve ownership etc/opensafely /etc/
+chmod 0755 /etc/opensafely/
 chmod 0640 /etc/opensafely/*
 
 ln -sf /etc/opensafely/profile /etc/profile.d/opensafely.sh
 ln -sf /etc/opensafely/sudoers /etc/sudoers.d/opensafely
 ln -sf /etc/opensafely/ssh.conf /etc/ssh/sshd_config.d/99-opensafely.conf
+
+# clean up old files
 rm -f /etc/opensafely/ssh-banner
 
+# explicitly allow these things for user to rx
 chmod 0755 /etc/opensafely/{banner,bashrc}
 
 # add login banner
-
 ln -sf /etc/opensafely/banner /etc/update-motd.d/00-opensafely-banner
+
 # add our bashrc config
 if ! grep -q "etc/opensafely/bashrc" /etc/bash.bashrc; then
     echo "test -f /etc/opensafely/bashrc && . /etc/opensafely/bashrc" >> /etc/bash.bashrc
 fi
 
 # reduce motd noise
-chmod a-x /etc/update-motd.d/{10-help-text,50-motd-news,90-updates-available,91-contract-ua-esm-status,91-release-upgrade}
+chmod a-x /etc/update-motd.d/{10-help-text,50-motd-news,91-contract-ua-esm-status,91-release-upgrade}
 
 grep -q "^UMASK.*027" /etc/login.defs || sed -i 's/^UMASK.*$/UMASK 027/' /etc/login.defs
 
