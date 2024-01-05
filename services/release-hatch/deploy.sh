eb8bba08fca0e734b0c842859651edcb06889053
bloodearnest <simon.davy@thedatalab.org>
2024-01-05 14:38:38 +0000

    Refactor release-hatch tooling
    This moves release-hatch to using a justfile to manage operations, like
    we did with job-runner.
    
    It also adds support for automatically pulling the latest LetsEncrypt
    certificates as part of the regular deployment, fixing #184.
    
    It formalises the previously adhoc method of cert management, namedly
    using relative sym links in the `certs/` subdir.  It also adds some
    protection to the test-only selfsigned cert generation code, so it
    should never happen in production.

--- services/release-hatch/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# start the docker container
-DIR=~opensafely/release-hatch
-echo "Pulling image"
-docker-compose --no-ansi -f $DIR/docker-compose.yaml pull --quiet release-hatch
-docker-compose --no-ansi -f $DIR/docker-compose.yaml up --detach release-hatch
