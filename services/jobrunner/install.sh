358beb298945de8eaba8448bac4d293f3186c561
bloodearnest <simon.davy@thedatalab.org>
2025-04-24 10:40:40 +0100

    Remove dev branch deploy to test backend

--- services/jobrunner/install.sh
+++ services/jobrunner/install.sh
@@ -24,11 +24,6 @@ cp $SRC_DIR/bin/* ~opensafely/bin/
 # setup some automated config for docker group id
 echo "DOCKER_HOST_GROUPID=$(getent group docker | awk -F: '{print $3}')" > $DIR/.env
 
-# run the dev container on the test-backend
-if [ "$BACKEND" = "test" ]; then
-  echo "JOB_RUNNER_DOCKER_IMAGE=job-runner-split" >> $DIR/.env
-fi
-
 chown -R opensafely:opensafely $DIR
 
 # TODO: this should probably live somewhere else, as its more than just a jobrunner thing
