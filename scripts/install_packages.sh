44675d53078ae0fcc8e8a5e379947bb0f2291fe2
David Evans <dave.evans@thedatalab.org>
2025-06-03 15:34:56 +0100

    Remove default apt-upgrade and replace with big shouty notice

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -6,8 +6,17 @@ set -euo pipefail
 set +u
 sed 's/^#.*//' purge-packages.txt | xargs apt-get purge -y
 apt-get update
-apt-get upgrade -y
 sed 's/^#.*//' core-packages.txt | xargs apt-get install --no-install-recommends -y
 sed 's/^#.*//' packages.txt | xargs apt-get install --no-install-recommends -y
 apt-get autoremove -y
 set -u
+
+if apt list --upgradable 2>/dev/null | grep -q "upgradable from"; then
+  echo "=============================== APT UPDATES AVAILABLE ==============================="
+  echo
+  echo "Newer versions of apt packages are available; you should run:"
+  echo
+  echo "    just apt-upgrade"
+  echo
+  echo "====================================================================================="
+fi
