05849957e3bd607e844fbcc21d7df082c10dec61
Tom Ward <tomward@fmail.co.uk>
2025-06-11 14:33:54 +0100

    Remove vmp-mapping timer test

--- tests/backends/tpp.sh
+++ tests/backends/tpp.sh
@@ -39,20 +39,3 @@ systemctl status collector || { journalctl -u collector --no-pager; exit 2; }
 
 # run airlock tests
 ./tests/check-airlock.sh
-
-msg="Wrong scheme for MS-SQL URL"
-rc=0
-
-# we need cohortextractor to run the vmp-mapping test
-just -f ~opensafely/jobrunner/justfile update-docker-image cohortextractor
-
-
-echo "We expect this to error in tests!"
-systemctl start --now vmp-mapping || rc=$?
-
-if ! journalctl -u vmp-mapping | grep -q 'Wrong scheme for MS-SQL URL'; then
-    echo "vmp-mapping should exit with exit code 1: exited with $rc"
-    echo "Expected error message not found: $msg"
-    journalctl -u vmp-mapping
-    exit 1
-fi
