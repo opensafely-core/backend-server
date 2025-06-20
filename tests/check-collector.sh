0b22cfa14c1fb161649b0c77949d9638275de733
bloodearnest <simon.davy@thedatalab.org>
2025-06-20 11:27:29 +0100

    Send trace telemetry via the local collector
    This configures the collector to receive and forward trace telemetry to
    honeycomb, and points the agent/airlock at it.
    
    We need to use the docker bridge IP so that they can talk to it from
    within the docker containers.

--- tests/check-collector.sh
+++ tests/check-collector.sh
@@ -7,6 +7,10 @@ err() {
 }
 
 systemctl status --no-pager collector >/dev/null || err
+
+# check that the rap.backend attribute has been configured
 journalctl --no-pager -u collector | grep "rap.backend" > /dev/null || err
 
+# check that there is trace data from airlock/agent
+journalctl --no-pager -u collector | grep "TracesExporter" > /dev/null || err
 
