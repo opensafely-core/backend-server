b78bf8c9fa1994af79018a4cb6c5f3d49d02bda9
Becky Smith <becky.smith@thedatalab.org>
2026-07-10 15:00:47 +0100

    Update README to describe instance outbound rules

--- backends/emisv2/scripts/write_dummy_airlock_cert_key.sh
+++ backends/emisv2/scripts/write_dummy_airlock_cert_key.sh
@@ -4,14 +4,11 @@
 # pulls the image and starts airlock. It won't start properly if the airlock
 # cert key file is missing altogether, but it will start if it's there, even
 # if it's not valid. We add the file here so that we don't bake the key into
-# the AMI, and we rerun this script on instance start (see cloud-init-emis/user-data)
-# with the real key
+# the AMI, and we populate the real key on instance start (see cloud-init-emis/user-data)
 set -euo pipefail
 
-AIRLOCK_CERT_KEY="${1:-dummy-key-replaced-on-instance-start}"
-
 mkdir -p /home/opensafely/airlock/certs
-printf '%b\n' "$AIRLOCK_CERT_KEY" > /home/opensafely/airlock/certs/airlock.key
+echo "dummy-key-replaced-on-instance-launch" > /home/opensafely/airlock/certs/airlock.key
 
 # set permissions; ownership will be handled in the airlock install script
 chmod 600 /home/opensafely/airlock/certs/airlock.key
